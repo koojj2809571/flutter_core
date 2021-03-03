d18aa89fef3841f0d85a04b21c84a0b0e47b5ccb
jijian <2302472911@qq.com>
2021-03-03 13:32:53 +0800

    -优化String工具类/toast

--- lib/util/data_util.dart
+++ lib/util/data_util.dart
@@ -9,41 +9,56 @@ extension StringUtil on String {
   /// 非空判断包含空格
   bool get blank => this.empty || this.trim().isEmpty;
 
-  void log(){
+  void log() {
     print(this);
   }
 
-  void logDebug({String tag: LogUtil.TAG_TEST}){
+  void logDebug({String tag: LogUtil.TAG_TEST}) {
     LogUtil.log(tag: tag, text: this);
   }
 
-  String get MD5{
+  void toast({
+    Toast length = Toast.LENGTH_SHORT,
+    ToastGravity gravity = ToastGravity.BOTTOM,
+    Color backColor = Colors.black54,
+    Color textColor = Colors.white,
+  }) {
+    ToastUtils.showToast(
+      this,
+      length: length,
+      gravity: gravity,
+      backColor: backColor,
+      textColor: textColor,
+    );
+  }
+
+  String get MD5 {
     var content = new Utf8Encoder().convert(this);
     var digest = md5.convert(content);
     // 这里其实就是 digest.toString()
     return hex.encode(digest.bytes);
   }
-  
+
   List<int> get upperCaseIndex {
     List<int> indexes = [];
-    for(int i = 0; i < this.codeUnits.length; i++){
+    for (int i = 0; i < this.codeUnits.length; i++) {
       int value = this.codeUnits[i];
-      if(value >= 65 && value <= 90){
+      if (value >= 65 && value <= 90) {
         indexes.add(i);
       }
     }
     return indexes;
   }
 
-  String splitUpperCaseWith(String start, String split){
-    if(start == null || split == null) return null;
+  String splitUpperCaseWith(String start, String split) {
+    if (start == null || split == null) return null;
     List<int> indexes = this.upperCaseIndex;
-    if(indexes[0] != 0){
+    if (indexes[0] != 0) {
       indexes.insert(0, 0);
     }
 
     var temp = <String>[];
-    for(int i = 0; i < indexes.length; i++){
+    for (int i = 0; i < indexes.length; i++) {
       int end = i == indexes.length - 1 ? this.length : indexes[i + 1];
       temp.add(this.toLowerCase().substring(indexes[i], end));
     }
@@ -65,7 +80,7 @@ extension ListUtil on List {
     String result = "";
     String indentationStr = " " * indentation;
     result += "$indentationStr[";
-    if(this.isNotEmpty) {
+    if (this.isNotEmpty) {
       this.forEach((value) {
         if (value is Map) {
           var temp = value.mapToStructureString(indentation: indentation + 2);
@@ -94,15 +109,14 @@ extension MapUtil on Map {
     String result = "";
     String indentationStr = " " * indentation;
     result += "{";
-    if(this.isNotEmpty) {
+    if (this.isNotEmpty) {
       this.forEach((key, value) {
         if (value is Map) {
           var temp = value.mapToStructureString(indentation: indentation + 2);
           result += "\n$indentationStr" + "\"$key\" : $temp,";
         } else if (value is List) {
           result += "\n$indentationStr" +
-              "\"$key\" : ${value.listToStructureString(
-                  indentation: indentation + 2)},";
+              "\"$key\" : ${value.listToStructureString(indentation: indentation + 2)},";
         } else {
           String temp = (value is String) ? "\"$value\"," : "$value,";
           result += "\n$indentationStr" + "\"$key\" : " + temp;
